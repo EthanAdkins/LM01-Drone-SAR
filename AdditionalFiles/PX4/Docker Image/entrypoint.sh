bcc4a8a5a63b9138264d5105693fca335787f2cf
Nick Brown <njbrown09@gmail.com>
2023-11-28 18:16:28 -0500

    Revert "Work on multiple px4"
    This reverts commit dcd94f355db7b6523417140a96ca5d86a4ecb555.

--- AdditionalFiles/PX4/Docker Image/entrypoint.sh
+++ AdditionalFiles/PX4/Docker Image/entrypoint.sh
@@ -2,4 +2,4 @@
 export PX4_SIM_HOST_ADDR=$1
 #socat UDP4-RECVFROM:5600,fork UDP4-SENDTO:${HOST_IP}:5600 &#!/bin/bash
 python3 /usr/local/bin/mavproxy.py --daemon --master=udpin:127.0.0.1:14550 --out=tcpin:0.0.0.0:5760 2>&1 > /dev/null &
-cd /src/PX4-Autopilot && HEADLESS=1 make px4_sitl_default none_iris --mavlink_tcp_port $2 --mavlink_udp_port $3 --mavlink_id $4
+cd /src/PX4-Autopilot && HEADLESS=1 make px4_sitl_default none_iris
