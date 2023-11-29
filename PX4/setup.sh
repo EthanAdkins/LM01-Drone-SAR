eb4f07c5ae65b16bb4bf72aeb1203dfc60880cba
Nick Brown <njbrown09@gmail.com>
2023-11-29 14:21:32 -0500

    Script Fix

--- PX4/setup.sh
+++ PX4/setup.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-
-# Install QGroundControl dependencies:
-sudo usermod -a -G dialout $USER
-sudo apt-get remove modemmanager -y
-sudo apt install gstreamer1.0-plugins-bad gstreamer1.0-libav gstreamer1.0-gl -y
-sudo apt install libqt5gui5 -y
-sudo apt install libfuse2 -y
+# /mnt/c/Users/Admin/Documents/Colosseum/PythonClient/multirotor/test/PX4
+sudo apt-get update
 
 # Clone PX4
 echo -e "\e[1;34mCloning PX4 Autopilot repository...\e[0m"
@@ -14,8 +9,6 @@ git clone https://github.com/PX4/PX4-Autopilot.git --recursive
 # Install PX4 Dependencies
 echo -e "\e[1;34mInstalling PX4 dependencies...\e[0m"
 bash ./PX4-Autopilot/Tools/setup/ubuntu.sh --no-nuttx --no-sim-tools
-
-exit
 
 # Compile PX4 without running
 echo -e "\e[1;34mCompiling PX4... This can take up to an hour.\e[0m"
