d91bb8b16ecee82b4f5327971446c1201f234ccf
Nick Brown <njbrown09@gmail.com>
2023-11-29 13:23:08 -0500

    More compose work

---
+++ AdditionalFiles/PX4/run_px4.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+# Get the directory where the script is located
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+# Function to ask user for the number of containers with a maximum limit of 10
+ask_for_container_count() {
+    while true; do
+        echo "Enter the number of containers (max 10):"
+        read container_count
+
+        # Check if input is an integer and less than or equal to 10
+        if [[ "$container_count" =~ ^[0-9]+$ ]] && [ "$container_count" -le 10 ]; then
+            break
+        else
+            echo "Please enter a valid number (1-10)."
+        fi
+    done
+}
+
+ask_for_container_count
+
+# Start the Docker Compose file
+cat <<EOF > docker-compose.yml
+version: '3.8'
+
+services:
+EOF
+
+# Function to generate container configuration
+generate_container_config() {
+    local container_number=$1
+    local udp_port=$((14570 + container_number))
+    local tcp_port=$((5760 + container_number))
+    local instance=$((container_number - 1))
+
+    cat <<EOF
+  px4-dev-ros-$container_number:
+    image: njbrown09/lm01-drone-sar:latest
+    privileged: true
+    stdin_open: true
+    tty: true
+    environment:
+      - PX4_HOME_LAT=42.3898
+      - PX4_HOME_LON=-71.1476
+      - PX4_HOME_ALT=14.2
+      - PX4_INSTANCE=$instance
+      - PX4_SIM_HOST_ADDR=172.23.0.1
+      - DISPLAY=:0
+    volumes:
+      - ${SCRIPT_DIR}/PX4-Autopilot:/src/PX4-Autopilot:rw
+      - /tmp/.X11-unix:/tmp/.X11-unix:ro
+    ports:
+      - "$udp_port:$udp_port/udp"
+      - "$tcp_port:$tcp_port/tcp"
+    networks:
+      - px4_network
+
+EOF
+}
+
+# Generate configurations for each container
+for (( i=1; i<=container_count; i++ ))
+do
+    generate_container_config $i >> docker-compose.yml
+done
+
+# Add network configuration
+cat <<EOF >> docker-compose.yml
+networks:
+  px4_network:
+    driver: bridge
+EOF
+
+echo "Docker Compose file generated: docker-compose.yml"
+
+# Create containers
+echo -e "\e[1;32mCreating containers using Docker Compose...\e[0m"
+docker-compose -f docker-compose.yml up
---
+++ AdditionalFiles/PX4/setup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Ensure the script is run with sudo
+if [ "$(id -u)" != "0" ]; then
+   echo -e "\e[1;31mThis script must be run as root (use sudo).\e[0m" 1>&2
+   #exit 1
+fi
+
+# Install QGroundControl dependencies:
+sudo usermod -a -G dialout $USER
+sudo apt-get remove modemmanager -y
+sudo apt install gstreamer1.0-plugins-bad gstreamer1.0-libav gstreamer1.0-gl -y
+sudo apt install libqt5gui5 -y
+sudo apt install libfuse2 -y
+
+# Clone PX4
+echo -e "\e[1;34mCloning PX4 Autopilot repository...\e[0m"
+git clone https://github.com/PX4/PX4-Autopilot.git --recursive
+
+# Install PX4 Dependencies
+echo -e "\e[1;34mInstalling PX4 dependencies...\e[0m"
+bash ./PX4-Autopilot/Tools/setup/ubuntu.sh --no-nuttx --no-sim-tools
+
+exit
+
+# Compile PX4 without running
+echo -e "\e[1;34mCompiling PX4... This can take up to an hour.\e[0m"
+cd PX4-Autopilot
+DONT_RUN=1 make px4_sitl_default none_iris
+cd ..
+
+# Output message
+echo -e "\e[1;32mPX4 Successfully Installed.\e[0m"
