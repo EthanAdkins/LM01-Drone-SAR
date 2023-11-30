a0025d489f41db18920a8c77afae3b5757b51eb1
Nick Brown <njbrown09@gmail.com>
2023-11-29 21:15:01 -0500

    More Work on Docker

--- PX4/Docker Image/entrypoint.sh
+++ PX4/Docker Image/entrypoint.sh
@@ -11,11 +11,11 @@ python3 /usr/local/bin/mavproxy.py --daemon --master=udpin:127.0.0.1:14550 --out
 build_path=/src/PX4-Autopilot/build/px4_sitl_default
 export PX4_SIM_MODEL=none_iris
 
-working_dir="$build_path/instance_$n"
+working_dir="$build_path/instance_$instance"
 [ ! -d "$working_dir" ] && mkdir -p "$working_dir"
 pushd "$working_dir" &>/dev/null
 
-echo "starting instance $n in $(pwd)"
+echo "starting instance $instance in $(pwd)"
 $build_path/bin/px4 -i $instance -d "$build_path/etc"
 popd &>/dev/null
 
