aaea99e144df90ed021c33d76d5354209dcb12ca
EthanAdkins <102610253+EthanAdkins@users.noreply.github.com>
2024-02-23 13:50:33 -0500

    Fixed Incorrect Latitude/Longitude Use
    I believe I had swapped them around. Fixed now though

--- DroneSwarm/BayesTheorem.py
+++ DroneSwarm/BayesTheorem.py
@@ -7,7 +7,7 @@ BOTTOM_LEFT_BOUND = configDrones.BOTTOM_LEFT_BOUND
 TOP_RIGHT_BOUND = configDrones.TOP_RIGHT_BOUND
 grid_index_width = configDrones.grid_index_width
 grid_index_height = configDrones.grid_index_height
-Grid = None
+global Grid
 
 def startBayes(size):
     #print(gridIndexWidth, "  ", gridIndexHeight)
@@ -42,16 +42,17 @@ def update_grid_with_evidence(Grid, evidence_grid):
 def gps_to_grid(latitude, longitude):
     """ Convert the GPS coordinate to the appropriate location in the Bayes grid"""
     print(((grid_index_width)))
-    xIndex = math.floor(((longitude - BOTTOM_LEFT_BOUND[1]) / grid_index_height))       # cell = (row, column) so this may seem reversed 
-    yIndex = math.floor(((latitude - BOTTOM_LEFT_BOUND[0]) / grid_index_width))   
+
+    xIndex = math.floor(((longitude - BOTTOM_LEFT_BOUND[1]) / grid_index_width))       # cell = (row, column) so this may seem reversed 
+    yIndex = math.floor(((latitude - BOTTOM_LEFT_BOUND[0]) / grid_index_height))   
     return (xIndex, yIndex)
 
 if __name__ == '__main__':
     size = (10, 20) # (y,x)
     #startBayes(size, (0.00014477851375283054,-0.0041943628509558415), (0.004516586943213801, 0.004839834080485371))
-    startBayes(size)
-    GPSCoord = (0.0003863109910887481,0.0016887219677169038)
-    #print(grid)
+    grid = startBayes(size)
+    GPSCoord = (0.0024477851375283054,-0.0021943628509558415) # Must be represented as (latitude, longitude)
+    print(grid)
     gridvalue = gps_to_grid(GPSCoord[0], GPSCoord[1])
     print(gridvalue)
     print('test')
