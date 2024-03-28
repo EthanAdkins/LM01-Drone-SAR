4a4e1e83180e30b698dd5fa369245d7bd41f56b1
Nick Brown <njbrown09@gmail.com>
2024-03-28 15:49:21 -0400

    Working!

--- DroneSwarm/MapHandler.py
+++ DroneSwarm/MapHandler.py
@@ -1,7 +1,6 @@
 import airsim
-import websockets
-import asyncio
 import numpy as np
+import airsim
 import math
 from HelperFunctions import clusterHelper
 import os
@@ -12,14 +11,19 @@ import time
 import json
 # import Constants
 import Constants.ros as ros
-from threading import Timer # Use for interval checks with minimal code
-from threading import Thread # USe for important code running constantly
+from threading import Timer  # Use for interval checks with minimal code
+from threading import Thread  # USe for important code running constantly
 from std_msgs.msg import String
 from std_srvs.srv import Trigger, TriggerResponse
 from airsim_ros_pkgs.msg import droneData
 from airsim_ros_pkgs.msg import updateMap
 from airsim_ros_pkgs.srv import getDroneData, getDroneDataResponse
 import threading
+
+import asyncio
+import websockets
+import threading
+
 god = threading.Lock()
 
 DETECTION_OUTPUT_SERIES = configDrones.DETECTION_OUTPUT_SERIES
@@ -27,13 +31,12 @@ ZOOM_FACTOR = configDrones.ZOOM_FACTOR
 MAX_PIX_COUNT = configDrones.MAX_PIX_COUNT
 SPIRAL_LOCATION_1 = [configDrones.SPIRAL_LOCATION_1[1], configDrones.SPIRAL_LOCATION_1[0]]
 
-
 # Environmental Variables
 # ros: topics
 MAP_HANDLER_TOPIC = ros.MAP_HANDLER_TOPIC
 FINAL_TARGET_POSITION = ros.FINAL_TARGET_POSITION
 NEW_GPS_PREDICTION = ros.NEW_GPS_PREDICTION
-UPDATE_DRONE_POSITION =  ros.UPDATE_DRONE_POSITION
+UPDATE_DRONE_POSITION = ros.UPDATE_DRONE_POSITION
 
 PREVIOUS_GPS_POSITIONS = []
 PREVIOUS_OVERSEER_GPS_POSITIONS = None
@@ -42,273 +45,55 @@ BATCH_TIME = None
 START_TIME = None
 WOLF_COUNT = None
 
-#Minimap!
 
-# WebSocket server URL
+# Global variable to store the WebSocket connection
+websocket_connection = None
 WEBSOCKET_SERVER_URL = "ws://172.23.0.1:8765"
 
+# Event loop for asynchronous tasks
+loop = asyncio.get_event_loop()
 
-async def register_drone(drone_name, websocket):
-    """Send a drone registration message to the server."""
-    drone_registration_message = {
-        "client_type": "drone_swarm",  # Identifies this client as part of the drone swarm
-        "message_type": "CreateDrone",
-        "drone_name": drone_name
-    }
-    await websocket.send(json.dumps(drone_registration_message))
-    print(f"Registered drone: {drone_name}")
+async def connect_to_websocket_server(uri):
+    global websocket_connection
+    websocket_connection = await websockets.connect(uri)
+    print("Connected to WebSocket server at:", uri)
+    # Construct and send the authentication message
+    auth_message = json.dumps({"client_type": "swarm", "message_type": "authentication"})
+    await websocket_connection.send(auth_message)
+    print("Authentication message sent.")
 
-async def send_auth(websocket):
-    """Send a drone registration message to the server."""
-    drone_auth_message = {
-        "client_type": "drone_swarm",  # Identifies this client as part of the drone swarm
-        "message_type": "authentication"
-    }
-    await websocket.send(json.dumps(drone_auth_message))
-    print(f"Authenticated")
+def start_websocket_connection(uri=WEBSOCKET_SERVER_URL):  # Ensure URI matches your server
+    # Run the connection in the event loop
+    loop.run_until_complete(connect_to_websocket_server(uri))
 
-async def update_drone(drone_name, latitude, longitude, altitude, velocity, websocket):
-    """Send a drone registration message to the server."""
-    drone_registration_message = {
-        "client_type": "drone_swarm",  # Identifies this client as part of the drone swarm
-        "message_type": "UpdateDrone",
-        "drone_name": drone_name,
-        "latitude": latitude,
-        "longitude": longitude,
-        "altitude": altitude,
-        "velocity": velocity,
-    }
-    await websocket.send(json.dumps(drone_registration_message))
-    print(f"Updated drone: {drone_name}")
+async def send_message_async(message):
+    global websocket_connection
+    if websocket_connection:
+        await websocket_connection.send(message)
+    else:
+        print("WebSocket connection is not established.")
 
+def send_message(message):
+    # Wrapper to send a message through the WebSocket from synchronous code
+    asyncio.run_coroutine_threadsafe(send_message_async(message), loop)
 
-async def connect_and_register_drones(drone_names):
-    """Connect to the WebSocket server and register each drone."""
-    try:
-        async with websockets.connect(WEBSOCKET_SERVER_URL) as websocket:
-            send_auth(websocket)
-            for drone_name in drone_names:
-                await register_drone(drone_name, websocket)
-            # Keep the connection open to listen for server messages or send updates
-            await websocket.wait_closed()
-    except Exception as e:
-        print(f"Connection failed: {e}")
-
-def start_websocket_client(drone_names):
-    """Starts the WebSocket client in an asyncio event loop."""
-    asyncio.new_event_loop().run_until_complete(connect_and_register_drones(drone_names))
-
-
-
+# Main Process Start ----------------------------------------------
 def startMapHandler(wolfCount):
     print("Starting map handler")
+    # Initiate WebSocket connection
+    start_websocket_connection()
 
     global BATCH_TIME, START_TIME, WOLF_COUNT
     WOLF_COUNT = wolfCount
     BATCH_TIME = time.time()
     START_TIME = time.time()
-
-    # Assuming globalVarSetup is a function you have defined to setup global variables
+    # Sets up empty arrays
     globalVarSetup(wolfCount)
 
-    # Simulating drone names based on the number of drones (wolves)
-    drone_names = [f"Drone_{i + 1}" for i in range(wolfCount)]
-
-    # Start the WebSocket client in a separate thread to avoid blocking ROS
-    threading.Thread(target=start_websocket_client, args=(drone_names,)).start()
-
-    # Create Node for "ProximityOverseer" (Only one should exist)
     nodeName = "MapHandler"
     rospy.init_node(nodeName, anonymous=True)
-
-    # Subscribe to map handler topic
-    print(f"Subscribing to {MAP_HANDLER_TOPIC}")
-    mapHandlerSub()  # Assuming this is a function you have defined to subscribe to ROS topics
 
 
 def globalVarSetup(droneCount):
     global PREVIOUS_GPS_POSITIONS
-    PREVIOUS_GPS_POSITIONS = [None]*droneCount
-
-
-# Connects subcriber listen to MapHandler
-def mapHandlerSub():
-    rospy.Subscriber(MAP_HANDLER_TOPIC, updateMap, updateResponseQ, ())
-    # rospy.Subscriber(MAP_HANDLER_TOPIC, updateMap, updateMapHandler, ())
-    rospy.spin()
-
-def updateResponseQ(data, args):
-
-    updateMapHandler(data)
-    print("Update Response Q")
-    print(data)
-
-    # Extracting wolfPosition
-    wolf_position = data.get('wolfPosition', {})
-
-    # Printing wolfPosition variables independently
-    latitude = wolf_position.get('latitude', 0)
-    longitude = wolf_position.get('longitude', 0)
-    altitude = wolf_position.get('altitude', 0)
-
-    print(f"Wolf Position Latitude: {latitude}")
-    print(f"Wolf Position Longitude: {longitude}")
-    print(f"Wolf Position Altitude: {altitude}")
-
-def updateMapHandler(data, detectMap):
-    global PREVIOUS_GPS_POSITIONS, PREVIOUS_OVERSEER_GPS_POSITIONS
-    # Gets the update type from the data
-    updateMapType = data.updateType
-
-    # Get the tuples
-    wolfPositionTuple = GPSToTuple(data.wolfPosition)
-    targetPositionTuple = GPSToTuple(data.targetPosition)
-
-    # Add true target position
-    if (updateMapType == FINAL_TARGET_POSITION):
-        targetPositionTuple = GPSToTuple(data.targetPosition)
-        handleTargetPosition(finalTargetPosition=targetPositionTuple, detectMap=detectMap)
-
-    # Add gps prediction
-    elif (updateMapType == NEW_GPS_PREDICTION):
-        # Gets necessary variables
-        wolfPositionTuple = GPSToTuple(data.wolfPosition)
-        predictedPosition = GPSToTuple(data.targetPosition)
-        imageNumber = data.imageNumber
-        vehicleName = data.wolfNumber
-        isOverseer = data.isOverseer
-
-        previousGPSPostion = PREVIOUS_OVERSEER_GPS_POSITIONS
-        if (not isOverseer):
-            previousGPSPostion = PREVIOUS_GPS_POSITIONS[vehicleName]
-
-        # Calls drone and gps update functions
-        handleDronePosition(lastWolfPos=previousGPSPostion, wolfPos=wolfPositionTuple, detectMap=detectMap, vehicleName=vehicleName, isOverseer=isOverseer)
-        handleGPSPrediction(wolfPos=wolfPositionTuple, predPos=predictedPosition, imageNumber=imageNumber, vehicleName=vehicleName, detectMap=detectMap, isOverseer=isOverseer)
-
-        # Updates previous gps into array
-        if (isOverseer):
-            PREVIOUS_OVERSEER_GPS_POSITIONS = wolfPositionTuple
-        else:
-            PREVIOUS_GPS_POSITIONS[vehicleName] = wolfPositionTuple
-
-    # Add drone position
-    elif (updateMapType == UPDATE_DRONE_POSITION):
-        # Gets necessary variables
-        wolfPositionTuple = GPSToTuple(data.wolfPosition)
-        vehicleName = data.wolfNumber
-        isOverseer = data.isOverseer
-
-        previousGPSPostion = PREVIOUS_OVERSEER_GPS_POSITIONS
-        if (not isOverseer):
-            previousGPSPostion = PREVIOUS_GPS_POSITIONS[vehicleName]
-
-        # Calls drone and gps update functions
-        handleDronePosition(lastWolfPos=previousGPSPostion, wolfPos=wolfPositionTuple, detectMap=detectMap, vehicleName=vehicleName, isOverseer=isOverseer)
-
-        # Updates previous gps into array
-        if (isOverseer):
-            PREVIOUS_OVERSEER_GPS_POSITIONS = wolfPositionTuple
-        else:
-            PREVIOUS_GPS_POSITIONS[vehicleName] = wolfPositionTuple
-
-# Converts tuples
-def GPSToTuple(GPS):
-    return [GPS.latitude, GPS.longitude]
-
-def handleTargetPosition(finalTargetPosition, detectMap):
-    # path = calcPath()
-    positionMapCenter = SPIRAL_LOCATION_1
-    pixCount = MAX_PIX_COUNT
-    FUDGE_FACTOR = ZOOM_FACTOR
-
-    # calculate lat and lon offset
-    LAT_OFFSET = (pixCount/2) - positionMapCenter[0]*FUDGE_FACTOR
-    LON_OFFSET = (pixCount/2) - positionMapCenter[1]*FUDGE_FACTOR
-
-    # if os.path.exists(path):
-    #     detectMap = cv2.imread(path)
-    # else:
-    #     print("Remove image handleTargetPosition-------------------------------------------------------------------------------------------------------------------");
-    #     detectionMap = np.zeros((pixCount, pixCount, 3))
-    #     cv2.imwrite(path, detectionMap)
-    #     detectMap = cv2.imread(path)
-
-    cv2.circle(detectMap, (int(finalTargetPosition[0]*FUDGE_FACTOR + LAT_OFFSET), abs(int(finalTargetPosition[1]*FUDGE_FACTOR + LON_OFFSET))), 5, (240, 32, 160), 2) # draw ground truth postion
-    # cv2.imwrite(path, detectMap)
-
-
-def handleGPSPrediction(wolfPos, predPos, imageNumber, vehicleName, detectMap, isOverseer):
-    # path = calcPath()
-    positionMapCenter = SPIRAL_LOCATION_1
-    pixCount = MAX_PIX_COUNT
-    FUDGE_FACTOR = ZOOM_FACTOR
-
-    # calculate lat and lon offset
-    LAT_OFFSET = (pixCount/2) - positionMapCenter[0]*FUDGE_FACTOR
-    LON_OFFSET = (pixCount/2) - positionMapCenter[1]*FUDGE_FACTOR
-
-    # if os.path.exists(path):
-    #     print("Path exists, doing read")
-    #     detectMap = cv2.imread(path)
-    # else:
-    #     print("Remove image handleGPSPrediction-------------------------------------------------------------------------------------------------------------------");
-    #     detectionMap = np.zeros((pixCount, pixCount, 3))
-    #     cv2.imwrite(path, detectionMap)
-    #     detectMap = cv2.imread(path)
-    predictedColor = (15, 155, 228)
-    lineWeight = 3
-    textPred = 'O_' + str(imageNumber)
-    textPredColor = (255, 255, 255)
-    if not isOverseer:
-        predictedColor = (255, 0, 0)
-        lineWeight = 2
-        textPred = str(imageNumber) + 'w' + str(vehicleName)
-        textPredColor = (0, 0, 255)
-        if (imageNumber < 0): # fail prediction
-            predictedColor = (0,255,255)
-
-    cv2.line(detectMap, (int(wolfPos[0]*FUDGE_FACTOR + LAT_OFFSET), abs(int(wolfPos[1]*FUDGE_FACTOR + LON_OFFSET))), (int(predPos[0]*FUDGE_FACTOR + LAT_OFFSET), abs(int(predPos[1]*FUDGE_FACTOR + LON_OFFSET))), (255, 255, 0), 2) # draw line between wolf and prediction
-    cv2.circle(detectMap, (int(wolfPos[0]*FUDGE_FACTOR + LAT_OFFSET), abs(int(wolfPos[1]*FUDGE_FACTOR + LON_OFFSET))), 2, (0, 0, 255), 2) # draw vehicle postion
-    cv2.circle(detectMap, (int(predPos[0]*FUDGE_FACTOR + LAT_OFFSET), abs(int(predPos[1]*FUDGE_FACTOR + LON_OFFSET))), lineWeight, predictedColor, lineWeight) # draw predicted gps postion for wolf
-    cv2.putText(detectMap, textPred, (int(wolfPos[0]*FUDGE_FACTOR + LAT_OFFSET) + 1, abs(int(wolfPos[1]*FUDGE_FACTOR + LON_OFFSET)) + 1), cv2.FONT_HERSHEY_DUPLEX, 0.5, (0, 0, 255), 1)
-    # cv2.imwrite(path, detectMap)
-
-def handleDronePosition(lastWolfPos, wolfPos, detectMap, vehicleName, isOverseer):
-    # path = calcPath()
-    positionMapCenter = SPIRAL_LOCATION_1
-    pixCount = MAX_PIX_COUNT
-    FUDGE_FACTOR = ZOOM_FACTOR
-    global WOLF_COUNT
-
-
-
-
-
-    # calculate lat and lon offset
-    LAT_OFFSET = (pixCount/2) - positionMapCenter[0]*FUDGE_FACTOR
-    LON_OFFSET = (pixCount/2) - positionMapCenter[1]*FUDGE_FACTOR
-
-    # if os.path.exists(path):
-    #     detectMap = cv2.imread(path)
-    # else:
-    #     detectionMap = np.zeros((pixCount, pixCount, 3))
-    #     cv2.imwrite(path, detectionMap)
-    #     detectMap = cv2.imread(path)
-
-    # if type(detectMap) == None or detectMap.size == 0:
-    #     print("READ ERRORRRRRRRRRRRRRRR")
-    #     return
-
-    droneColor = (255, 255, 255)
-    lineWeight = 2
-    if not isOverseer:
-        droneColor = (255*( 1 - vehicleName/WOLF_COUNT), 140, 255*(vehicleName/WOLF_COUNT))
-        lineWeight = 1
-
-    print("New Wolf Pos:")
-    print(vehicleName)
-    print(wolfPos)
-
-def calcPath():
-    return '/home/testuser/AirSim/PythonClient/multirotor/Drone-Search-and-Rescue-SD/DroneSwarm/detectionMaps' + '/' + 'detections' + '_' + DETECTION_OUTPUT_SERIES + '_' + '.jpeg'
+    PREVIOUS_GPS_POSITIONS = [None] * droneCount
--- Minimap/Minimap Server/websocket_server.py
+++ Minimap/Minimap Server/websocket_server.py
@@ -3,41 +3,80 @@ import json
 import websockets
 from drone_monitor import DroneMonitor  # Ensure this path is correct
 
-connected = set()
+# Define separate sets for different types of clients
+minimap_clients = set()
+swarm_clients = set()
 
 
-async def broadcast(message):
-    if connected:
-        await asyncio.wait([client.send(message) for client in connected])
+async def broadcast_to_all(message):
+    await asyncio.wait([client.send(message) for client in minimap_clients.union(swarm_clients)])
+
+
+async def broadcast_to_minimap(message):
+    if minimap_clients:
+        await asyncio.wait([client.send(message) for client in minimap_clients])
+
+
+async def broadcast_to_swarm(message):
+    if swarm_clients:
+        await asyncio.wait([client.send(message) for client in swarm_clients])
 
 
 async def register(websocket, drone_monitor):
-    connected.add(websocket)
-    # Now calling the function to send CreateDrone messages for each drone directly
-    await drone_monitor.send_drone_creation_messages(websocket)
+    print(f"Registering new connection from {websocket.remote_address}")
+    # Initially, just add to a general connected set; categorization happens upon authentication
+    # await drone_monitor.send_drone_creation_messages(websocket)
 
 
 async def unregister(websocket):
-    connected.remove(websocket)
+    print(f"Unregistering connection from {websocket.remote_address}")
+    minimap_clients.discard(websocket)
+    swarm_clients.discard(websocket)  # Remove from both sets to ensure clean up
+
+
+async def handle_authentication(websocket, data):
+    client_type = data.get("client_type")
+    if client_type == "minimap":
+        minimap_clients.add(websocket)
+        print(f"Minimap client connected: {websocket.remote_address}")
+    elif client_type == "swarm":
+        swarm_clients.add(websocket)
+        print(f"Swarm client connected: {websocket.remote_address}")
+    else:
+        print(f"Unknown client type: {client_type}")
+
+
+async def process_message(websocket, message):
+    try:
+        data = json.loads(message)
+        message_type = data.get("message_type")
+
+        if message_type == "authentication":
+            await handle_authentication(websocket, data)
+        else:
+            print(f"Unhandled message type: {message_type}")
+    except json.JSONDecodeError as e:
+        print(f"Error decoding message: {e}")
 
 
 async def websocket_server(websocket, path, drone_monitor):
     await register(websocket, drone_monitor)
     try:
-        await websocket.wait_closed()
+        async for message in websocket:
+            print(f"Received message from {websocket.remote_address}: {message}")
+            await process_message(websocket, message)
+    except websockets.exceptions.ConnectionClosed as e:
+        print(f"Connection closed with {websocket.remote_address}, reason: {e}")
+    except Exception as e:
+        print(f"Error processing message from {websocket.remote_address}: {e}")
     finally:
         await unregister(websocket)
 
 
 async def start_server(settingsFile):
-    # Create and setup the drone monitor
-    drone_monitor = DroneMonitor(broadcast)
-    drone_monitor.Setup(settingsFile)  # Now passing the entire settings file to Setup
+    drone_monitor = DroneMonitor(broadcast_to_all)
+    drone_monitor.Setup(settingsFile)
 
-    # Start monitoring drones in a background task
-    monitor_task = asyncio.create_task(drone_monitor.monitor_drones())
-
-    # Serve the WebSocket, passing drone_monitor to the server function
     async with websockets.serve(lambda ws, path: websocket_server(ws, path, drone_monitor), "0.0.0.0", 8765):
         await asyncio.Future()  # Run forever
 
@@ -47,4 +86,4 @@ if __name__ == "__main__":
         settingsFile = json.load(f)
     print("Current Vehicles:", list(settingsFile["Vehicles"].keys()))
 
-    asyncio.run(main(settingsFile))
+    asyncio.run(start_server(settingsFile))
